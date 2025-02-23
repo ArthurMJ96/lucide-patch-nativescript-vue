3882f8cd26aaae8f9e7a727b2cc78af8d11684e1
Arthur <45236453+ArthurMJ96@users.noreply.github.com>
2025-02-23 23:34:35 +0100

    Add initial project setup with package.json, webpack configuration, and .gitignore

---
+++ nativescript.webpack.js
@@ -0,0 +1,21 @@
+const webpack = require("@nativescript/webpack");
+const path = require("path");
+
+module.exports = (env) => {
+  webpack.init(env);
+
+  webpack.chainWebpack((config) => {
+    // Create a new rule for files coming from the specific package.
+    config.module
+      .rule("override-vue-for-server-renderer")
+      .test(/\.(js|vue)$/)
+      .include.add(
+        path.resolve(__dirname, "node_modules", "@vue", "server-renderer")
+      )
+      .end()
+      // Override the resolution so that any import of "vue" in @vue/server-renderer files points to the original Vue package
+      .resolve.alias.set("vue", path.resolve(__dirname, "node_modules/vue"));
+  });
+
+  return webpack.resolveConfig();
+};
